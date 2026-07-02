a5583d38a55f7a5698541e92c23ddd0cb6195a4e
Ramansahni <ramansahni1612@gmail.com>
2026-07-02 21:17:23 +0530

    Containerize Streamlit frontend with Docker

--- config/settings.py
+++ config/settings.py
@@ -1,9 +1,12 @@
+import os
+
 # General project configuration settings
 
 RANDOM_STATE = 42
 TEST_SIZE = 0.2
 N_ESTIMATORS = 500
 
-APP_ENV = 'development'
-DEBUG = True
-BACKEND_API_URL = "http://127.0.0.1:8000"
+APP_ENV = os.getenv('APP_ENV', 'development')
+DEBUG = os.getenv('DEBUG', 'True').lower() in ('true', '1', 't')
+BACKEND_API_URL = os.getenv('BACKEND_API_URL', "http://127.0.0.1:8000")
+
